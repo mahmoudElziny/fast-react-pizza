ad6b95218b82e14a255ab81f90d8d245a8079134
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-03-14 06:59:45 +0200

    styled 2 buttons links and loader

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,8 +2,10 @@
 // https://uibakery.io/regex-library/phone-number
 
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+
 import { createOrder } from "../../services/apiRestaurant";
 import { fakeCart, isValidPhone } from "../../utils/helpers";
+import Button from "../../ui/Button";
 
 
 
@@ -62,12 +64,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}
-            className="bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full
-            hover:bg-yellow-300 transition-colors duration-300
-            focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed">
+          <Button disabled={isSubmitting}>
             {isSubmitting ? 'Packing order....' : 'Order now'}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
