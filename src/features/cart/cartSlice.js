af3a9db5ecc80b8e69a11db8a19902a2bf0a7c6f
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-03-17 06:55:29 +0200

    update cart quantities

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -17,20 +17,20 @@ const cartSlice = createSlice({
             state.cart = state.cart.filter(item => item.pizzaId !== action.payload);
         },
         increaseItemQuantity(state, action) {
-            //payload = pizzaId
-            const item = state.cart.find(item => item.id === action.payload);
+            // payload = pizzaId
+            const item = state.cart.find((item) => item.pizzaId === action.payload);
+
             item.quantity++;
             item.totalPrice = item.quantity * item.unitPrice;
         },
         decreaseItemQuantity(state, action) {
-            //payload = pizzaId
-            const item = state.cart.find(item => item.id === action.payload);
+            // payload = pizzaId
+            const item = state.cart.find((item) => item.pizzaId === action.payload);
+
             item.quantity--;
-            if (item.quantity === 0) {
-                state.cart = state.cart.filter(item => item.id !== action.payload);
-            } else {
-                item.totalPrice = item.quantity * item.unitPrice;
-            }
+            item.totalPrice = item.quantity * item.unitPrice;
+
+            if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
         },
         clearCart(state) {
             state.cart = [];
@@ -45,6 +45,6 @@ export default cartSlice.reducer;
 export const getCart = (state) => state.cart.cart;
 export const getTotalCartQuantity = (state) => state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 export const getTotalCartPrice = (state) => state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
-export const getCurrentQuantityById = (pizzaId) => (state) => state.cart.cart.find(item => item.pizzaId === pizzaId)?.quantity ?? 0;
-
+export const getCurrentQuantityById = (id) => (state) =>
+    state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
 //reselect library to opimize these selectors
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -11,6 +11,7 @@ export default function Button({ children, disabled, to, type, onClick }) {
     const styles = {
         primary: base + ' py-3 px-4 md:px-6 md:py-4',
         small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+        round: base + ' py-1 px-2.5 md:px-3.5 md:py-2 text-sm',
         secondary: `text-sm uppercase border-2 border-stone-300 py-2.5 px-4 md:px-6 md:py-3.5
     font-semibold text-stone-400 inline-block tracking-wide rounded-full
     hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300
