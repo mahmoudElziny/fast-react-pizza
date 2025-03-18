98ffaba9a48fc67d071502c258c260eaa495b770
Mahmoud Elzeny <mahmoudelziny20@gmail.com>
2025-03-18 03:29:50 +0200

    Integrating GeoLocation

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -20,7 +20,9 @@ function CreateOrder() {
 
   const [withPriority, setWithPriority] = useState(false);
 
-  const username = useSelector((state) => state.user.username);
+  const { username, address, status: addressStatus, position, error: errorAddress } = useSelector((state) => state.user);
+  const isLoadingAddress = addressStatus === 'loading';
+
 
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
@@ -39,8 +41,6 @@ function CreateOrder() {
     <div className="px-4 py-6">
       <h2 className="text-xl font-semibold mb-8">Ready to order? Let&apos;s go!</h2>
 
-      <button onClick={() => dispatch(fetchAddress())}>Get position</button>
-
       <Form method="POST" action="/order/new">
         <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
@@ -57,12 +57,25 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input type="text" name="address" required
+              disabled={isLoadingAddress}
+              defaultValue={address}
               className="input w-full" />
+            {addressStatus === 'error' && (
+              <p className="text-xs bg-red-100 text-red-700 mt-2 p-2 rounded-md">{errorAddress}</p>
+            )}
           </div>
+          {!position.latitude && !position.longitude && (
+            <span className="absolute right-[3px] top-[3px] z-50 md:right-[5px] md:top-[5px]">
+              <Button type='small' disabled={isLoadingAddress} onClick={(e) => {
+                e.preventDefault();
+                dispatch(fetchAddress());
+              }}>Get position</Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -80,8 +93,9 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-
-          <Button type="primary" disabled={isSubmitting}>
+          <input type="hidden" name="position"
+            value={position.longitude && position.latitude ? `${position.latitude},${position.longitude}` : ''} />
+          <Button type="primary" disabled={isSubmitting || isLoadingAddress}>
             {isSubmitting ? 'Packing order....' : `Order now from ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -57,9 +57,9 @@ const userSlice = createSlice({
       )
       .addCase(
         fetchAddress.rejected,
-        (state, action) => {
+        (state) => {
           state.status = 'error';
-          state.error = action.error.message;
+          state.error = 'There was a problem getting your address. Make sure to fill it manually';
         }
       ),
 });
